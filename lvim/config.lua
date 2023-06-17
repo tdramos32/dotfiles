d334cd674a67d01885ddf6aaee16c8e978796ec9
Tyler D. Ramos <tdramos32@gmail.com>
2023-06-17 00:31:54 -0400

    added python ide to lunar vim

--- lvim/config.lua
+++ lvim/config.lua
@@ -108,6 +108,11 @@ lvim.builtin.treesitter.auto_install = true
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
+    "ChristianChiarulli/swenv.nvim",
+    "stevearc/dressing.nvim",
+    "mfussenegger/nvim-dap-python",
+    "nvim-neotest/neotest",
+    "nvim-neotest/neotest-python",
     { 'nvim-pack/nvim-spectre' },
     { "jeffkreeftmeijer/vim-numbertoggle" },
     { "ThePrimeagen/harpoon" },
@@ -146,7 +151,7 @@ lvim.transparent_window = true
 --Spectre Config Default from Github readme
 lvim.builtin.which_key.mappings["r"] = {
     name = "Spectre",
-    o = { '<cmd>lua require("spectre").open()<CR>', "Open Spectre" },
+    r = { '<cmd>lua require("spectre").open()<CR>', "Open Spectre" },
     w = { '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', "Search current word" },
     p = { '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
         "Search on current file" },
@@ -154,4 +159,62 @@ lvim.builtin.which_key.mappings["r"] = {
 lvim.builtin.which_key.vmappings["r"] = {
     name = "Spectre",
     w = { '<esc><cmd>lua require("spectre").open_visual()<CR>', "Search current word" },
+}
+
+
+--PYTHON IDE
+
+-- automatically install python syntax highlighting
+lvim.builtin.treesitter.ensure_installed = {
+    "python",
+}
+
+-- setup formatting
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" }, }
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.py" }
+
+-- setup linting
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", filetypes = { "python" } } }
+
+-- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+    require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+-- setup testing
+require("neotest").setup({
+    adapters = {
+        require("neotest-python")({
+            -- Extra arguments for nvim-dap configuration
+            -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+            dap = {
+                justMyCode = false,
+                console = "integratedTerminal",
+            },
+            args = { "--log-level", "DEBUG", "--quiet" },
+            runner = "pytest",
+        })
+    }
+})
+
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>",
+    "Test Method" }
+lvim.builtin.which_key.mappings["dM"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+    "Test Method DAP" }
+lvim.builtin.which_key.mappings["df"] = {
+    "<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>", "Test Class" }
+lvim.builtin.which_key.mappings["dF"] = {
+    "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Test Class DAP" }
+lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
+
+
+-- binding for switching
+lvim.builtin.which_key.mappings["C"] = {
+    name = "Python",
+    c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
 }
