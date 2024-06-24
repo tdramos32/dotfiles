d1feffb0abe59e8b83e689ef5f8ec44a7a9755af
Tyler Ramos <tdramos32@gmail.com>
2024-06-24 15:19:17 -0400

    updated nvim plugin options

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,5 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+  "folke/tokyonight.nvim",
+  lazy = true,
+  opts = { style = "night" },
 }
