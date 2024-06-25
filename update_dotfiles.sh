b8ef57e6496c9071208c79e8654337384e8f68ab
Tyler Ramos <tdramos32@gmail.com>
2024-06-25 15:33:35 -0400

    adding new line to script to add bashrc

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -3,3 +3,4 @@
 cp ~/.tmux.conf ~/dotfiles/tmux/tmux.conf -v
 cp ~/.config/alacritty/alacritty.toml ~/.config/alacritty/tokyo-night.toml ~/dotfiles/alacritty/ -r -v
 cp ~/.config/nvim/lua/ ~/dotfiles/nvim/ -r -v
+cp ~/.bashrc ~/dotfiles/.bashrc -v
