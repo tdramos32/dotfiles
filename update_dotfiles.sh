de85e7e4067ea624a5053b2714096071bfa8321c
Tyler Ramos <tdramos32@gmail.com>
2024-06-24 20:23:19 -0400

    added script that auto copies my dotfiles and updates them

---
+++ update_dotfiles.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.tmux.conf ~/dotfiles/tmux/tmux.conf -v
+cp ~/.config/alacritty/alacritty.toml ~/.config/alacritty/tokyo-night.toml ~/dotfiles/alacritty/ -r -v
+cp ~/.config/nvim/lua/ ~/dotfiles/nvim/ -r -v
